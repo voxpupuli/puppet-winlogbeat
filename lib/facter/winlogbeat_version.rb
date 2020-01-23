bbfbbcbd7113203b1d911873c898da332d188906
langoureaux-s <linuxworkgroup@hotmail.com>
2020-01-23 12:37:03 +0000

    Try to fix factor

--- lib/facter/winlogbeat_version.rb
+++ lib/facter/winlogbeat_version.rb
@@ -1,10 +1,13 @@
 require 'facter'
 Facter.add('winlogbeat_version') do
-  confine :kernel => ['Windows'] # rubocop:disable Style/HashSyntax
+  confine 'kernel' => ['Windows']
   if File.exist?('c:\Program Files\Winlogbeat\winlogbeat.exe')
-    winlogbeat_version = Facter::Util::Resolution.exec('"c:\Program Files\Winlogbeat\winlogbeat.exe" --version')
+    winlogbeat_version = Facter::Util::Resolution.exec('"c:\Program Files\Winlogbeat\winlogbeat.exe" version')
+    if winlogbeat_version.empty?
+      winlogbeat_version = Facter::Util::Resolution.exec('"c:\Program Files\Winlogbeat\winlogbeat.exe" --version')
+    end
   end
   setcode do
-    %r{^winlogbeat version ([^\s]+)?}.match(winlogbeat_version)[1] unless winlogbeat_version.nil?
+    winlogbeat_version.nil ? false : %r{^winlogbeat version ([^\s]+)?}.match(winlogbeat_version)[1]
   end
 end
